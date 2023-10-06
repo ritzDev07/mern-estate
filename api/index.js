ed8ee2b71268fdc6f2861a1b35a4ac917635af3f
Ritz Dev <ritzocite@gmail.com>
2023-10-06 15:40:02 +1300

    Create middleware and a function to handle potential errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ app.listen(3000, () => {
 //route for API endpoints
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+// Middleware for handling errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Error!';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
