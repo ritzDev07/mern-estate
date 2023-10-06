b653c0901bd77ab41fa0df1cb3d2a51880ce2700
Ritz Dev <ritzocite@gmail.com>
2023-10-06 21:04:54 +1300

    Create Sign-in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -10,5 +12,33 @@ export const signup = async (req, res, next) => {
         res.status(201).json("User created succesfully!");
     } catch (error) {
         next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const validUser = await User.findOne({ email });
+        if (!validUser)
+            return next(errorHandler(404, 'User not found!'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword)
+            return next(errorHandler(401, 'Wrong Credentials'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        // Remove the password field from the user data
+        const { password: pass, ...rest } = validUser._doc;
+
+        // Set the 'access_token' cookie with the JWT, and make it HTTP-only Also, set the cookie to expire in 7 days
+        res.cookie('access_token', token, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+            .status(200)
+            .json(rest);
+
+    } catch (error) {
+        next(error)
     }
 };
