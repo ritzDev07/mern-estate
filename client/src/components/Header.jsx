e32b4c5f4c067e6820ea5e97943918a4c454ae2a
Ritz Dev <ritzocite@gmail.com>
2023-10-07 18:56:49 +1300

    Update the  Header and make the Profile Page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { IoSearch } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const { currentUser } = useSelector(state => state.user);
+
     const menuItems = [
         { path: '/', label: 'Home', hidden: true },
         { path: '/about', label: 'About', hidden: true },
-        { path: '/sign-in', label: 'Sign in', hidden: false },
+        {
+            path: '/profile',
+            label: currentUser ? (
+                <img
+                    src={currentUser.profilePicture}
+                    alt='profile'
+                    className=' inline-block h-9 w-9 rounded-full object-cover mr-2 hover:'
+                />
+            ) : (
+                'Sign in'
+            ),
+            hidden: false,
+        },
+
     ];
 
     const Logo = () => (
@@ -32,19 +48,21 @@ const Header = () => {
                     />
                     <IoSearch size={20} className=' text-slate-500' />
                 </form>
-                <ul className='flex'>
+                <ul className='flex items-center'>
+
                     {menuItems.map((item, index) => (
                         <Link to={item.path} key={index}>
                             <li
                                 className={`${item.hidden
                                     ? 'hidden sm:inline'
                                     : 'sm:inline'
-                                    } text-slate-700 hover:border-b-4 hover:border-green-500 pb-1 mr-3`}
+                                    } text-slate-700 hover:border-b-4 hover:text-green-500 hover:font-medium pb-1 mr-3`}
                             >
                                 {item.label}
                             </li>
                         </Link>
                     ))}
+
                 </ul>
             </div>
         </header>
